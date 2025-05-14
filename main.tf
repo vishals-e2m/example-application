97e166f7684c9d71ec3c71281020f130f72c85e6
vishals-e2m <vishal.suthar@e2m.solutions>
2025-05-14 16:44:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,1 @@
-# main.tf
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.1"
-    }
-  }
-}
 
-provider "null" {}
-
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo Hello from Scalr!"
-  }
-}
