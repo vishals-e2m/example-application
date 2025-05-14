5f2afb3f179c0ea797d6898cd1e723ffc5faf894
vishals-e2m <vishal.suthar@e2m.solutions>
2025-05-14 16:45:08 +0530

    Update shared-state.tf

--- shared-state.tf
+++ shared-state.tf
@@ -1,10 +1,1 @@
-data "terraform_remote_state" "vishalproject" {
-  backend = "remote"
-  config = {
-    hostname     = "vishal-company.scalr.io"
-    organization = "env-v0oqh6fgu19qe9nru"
-    workspaces = {
-      name = "example-appliaction"
-    }
-  }
-}
+
